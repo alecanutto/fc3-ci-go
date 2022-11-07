17dd7b9110d7b2209509b1fbdc30f6eaa5148a6f
Alessandra Canuto <ale.canutto@gmail.com>
2022-11-07 16:17:05 -0300

    feat: :sparkles: add simple sum function for test

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(Sum(10, 10))
+}
+
+func Sum(i1, i2 int) int {
+	return i1 + i2
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alecanutto/fc3-ci-go
+
+go 1.19
